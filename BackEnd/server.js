9d61143165e3302241bdbc10c60584b680b831bc
Shivam verma <shivamverma2001sj@gmail.com>
2025-04-26 20:08:10 +0530

    apply the changes for build

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,22 @@
 import app from "./app.js";
+import express from 'express';
 import connectDB from "./db.js";
 const PORT = process.env.PORT || 5000;
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+if (process.env.NODE_ENV === 'production') {
+  const staticPath = path.join(__dirname, '..', 'FrontEnd', 'dist');
+  app.use(express.static(staticPath));
+  
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(staticPath, 'index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
